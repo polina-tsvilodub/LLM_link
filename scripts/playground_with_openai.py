2a9b5c4b4c3a98af62ffa4ec45f7e9d6808c55de
Hening Wang <hening.wang0615@gmail.com>
2023-08-16 17:39:23 +0200

    add two playgrond scripts

---
+++ scripts/playground_with_openai.py
@@ -0,0 +1,20 @@
+import openai
+
+openai.api_key = "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"
+
+# Set the prompt you want the model to complete
+prompt = "Once upon a time, in a land far away,"
+
+# Call the OpenAI API to generate a completion
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=50,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+# Extract and print the generated text
+generated_text = response.choices[0].text.strip()
+print(generated_text)
